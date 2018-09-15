25e00a8a6c74d9717c2e6824f7ad579775025adb
Bryce Palmer <everettraven@gmail.com>
2018-09-15 19:15:40 -0400

    Add files via upload

---
+++ CoordinatesClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordinates
+{
+    class CoordinatesClass
+    {
+        //Coordinate class to store values of a coordinate
+        public class Coordinate
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+        }
+
+        //Declare the list of Coordinates
+        List<Coordinate> CoordList = new List<Coordinate>();
+
+        //Function to add a coordinate to the coordinate list
+        public void AddCoord(Coordinate Coord)
+        {
+            CoordList.Add(new Coordinate { x = Coord.x, y = Coord.y });
+        }
+
+        //Function to list the coordinates stored in the list
+        public void ListCoords()
+        {
+            foreach(var C in CoordList)
+            {
+                Console.WriteLine("({0}, {1})" ,C.x,C.y);
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordinates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CoordinatesClass c = new CoordinatesClass();
+                CoordinatesClass.Coordinate coordinate = new CoordinatesClass.Coordinate();
+                bool run = true;
+                while (run == true)
+                {
+
+                    Console.WriteLine("WELCOME TO THE BACKEND OF THE 'SECRET PROJECT'");
+                    Console.WriteLine();
+                    Console.WriteLine("------------------------------------------------------");
+                    Console.WriteLine();
+                    Console.WriteLine("GIVE ME THE COORDINATE VALUE OF X");
+                    coordinate.x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("GIVE ME THE COORDINATE VALUE OF Y ");
+                    coordinate.y = Convert.ToInt32(Console.ReadLine());
+                    c.AddCoord(coordinate);
+                    Console.WriteLine("COORDINATE ADDED");
+                    c.ListCoords();
+                    Console.WriteLine("TYPE END TO STOP THE PROGRAM");
+                    string end = Console.ReadLine();
+                    end = end.ToUpper();
+                    if(end == "END")
+                    {
+                        run = false;
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
